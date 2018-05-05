0e685ace12e369e93d3edd48d839794d66402550
8dspaces <mickqi1985@hotmail.com>
2018-05-05 08:44:58 +0800

    update

--- src/test/java/GoogleTest.java
+++ src/test/java/GoogleTest.java
@@ -31,7 +31,7 @@ public class GoogleTest extends BeforeTests{
     GooglePage page = open("http://www.baidu.com/", GooglePage.class);
     SearchResultsPage results = page.searchFor("selenide");
     //results.getResults().shouldHave(sizeGreaterThan(1));
-    results.getResult(0).shouldHave(text("xelenide"));
+    results.getResult(0).shouldHave(text("Selenide"));
   }
 
 }
